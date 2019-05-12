bc1f1b318aa62e5e20b3fe5a26f337add82d3c88
mtsgi <oq@live.jp>
2019-05-12 23:08:58 +0900

    再ログイン画面自動遷移に関連するバグの修正 [2.1]

--- cs.js
+++ cs.js
@@ -2,16 +2,12 @@ chrome.storage.sync.get( null, function( items ) {
     if( location.hostname.indexOf( items.cpex_domain ) != -1 ) {
 
         console.log("Init \"Cpex\" ");
-        
-        if( items.cpex_open == "true" && $(".cs_Bt")[1].value == "すべて開く" ){
-            $(".cs_Bt")[1].click();
-        }
-
-        if( items.cpex_disp_size ) $("#cp-mojisuu").css("font-size", String(items.cpex_disp_size) )
 
         if( location.pathname == "/lms/error/notLogin" || location.pathname == "/lms/error/" ) {
             location.pathname = "/lms/lginLgir/";
         }
+
+        if( items.cpex_disp_size ) $("#cp-mojisuu").css("font-size", String(items.cpex_disp_size) )
 
         if( items.cpex_disp_x ){
             if( items.cpex_disp_x == "left" ) $("#cp-mojisuu").css("left", "0").css("right", "auto");
@@ -31,5 +27,9 @@ chrome.storage.sync.get( null, function( items ) {
         } ).on( "blur", function() {
             $( "#cp-mojisuu" ).hide();
         } );
+        
+        if( items.cpex_open == "true" && $(".cs_Bt")[1].value == "すべて開く" ){
+            $(".cs_Bt")[1].click();
+        }
     }
 } );
