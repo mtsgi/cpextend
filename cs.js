1f649609b63b4069be7b33799d117554b5903593
mtsgi <oq@live.jp>
2019-05-12 23:02:18 +0900

    自動オープン機能の実装／オプション画面の表示形式を変更 [2.0]

--- cs.js
+++ cs.js
@@ -1,10 +1,25 @@
 chrome.storage.sync.get( null, function( items ) {
     if( location.hostname.indexOf( items.cpex_domain ) != -1 ) {
-        
-        console.log( "init" );
 
-        if( location.pathname == "lms/error/notLogin" ) {
+        console.log("Init \"Cpex\" ");
+        
+        if( items.cpex_open == "true" && $(".cs_Bt")[1].value == "すべて開く" ){
+            $(".cs_Bt")[1].click();
+        }
+
+        if( items.cpex_disp_size ) $("#cp-mojisuu").css("font-size", String(items.cpex_disp_size) )
+
+        if( location.pathname == "/lms/error/notLogin" || location.pathname == "/lms/error/" ) {
             location.pathname = "/lms/lginLgir/";
+        }
+
+        if( items.cpex_disp_x ){
+            if( items.cpex_disp_x == "left" ) $("#cp-mojisuu").css("left", "0").css("right", "auto");
+            else if( items.cpex_disp_x == "right" ) $("#cp-mojisuu").css("right", "0").css("left", "auto");
+        }
+        if( items.cpex_disp_y ){
+            if( items.cpex_disp_y == "top" ) $("#cp-mojisuu").css("top", "0").css("bottom", "auto");
+            else if( items.cpex_disp_y == "bottom" ) $("#cp-mojisuu").css("bottom", "0").css("top", "auto");
         }
 
         $( "body" ).append( "<div id='cp-mojisuu'></div>" );
--- opt.js
+++ opt.js
@@ -2,10 +2,23 @@
 $( function() {
     chrome.storage.sync.get(null, function(items) {       
         $("#domain").val( items.cpex_domain );
+        if( items.cpex_disp_x ) $("#disp-x").val( items.cpex_disp_x );
+        if( items.cpex_disp_y ) $("#disp-y").val( items.cpex_disp_y );
+        if( items.cpex_open ) $("#open").val( items.cpex_open );
+        if( items.cpex_disp_size ) $("#disp-size").val( items.cpex_disp_size );
     });
     $("#apply").on("click", function(){
         chrome.storage.sync.set({
-            cpex_domain : $("#domain").val()
+            cpex_domain : $("#domain").val(),
+            cpex_disp_x : $("#disp-x").val(),
+            cpex_disp_y : $("#disp-y").val(),
+            cpex_open : $("#open").val(),
+            cpex_disp_size : $("#disp-size").val()
         });
+        $("#popup-text").text("設定を適用しました。");
+        $("#popup").fadeIn(100);
     });
-} );
+    $("#popup-div a").on("click", function(){
+        $("#popup").fadeOut(100);
+    });
+});
