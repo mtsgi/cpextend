45a0db23dc4ca9e70efdf98a1b683cd006a5473f
mtsgi <oq@live.jp>
2019-06-25 20:26:16 +0900

    ウィジェット表示設定を追加／自動出席の挙動を変更 [2.6]

--- cs.js
+++ cs.js
@@ -3,27 +3,40 @@ chrome.storage.sync.get( null, function( items ) {
 
         console.log("Init \"Cpex\" ");
 
+        $( "body" ).append( "<div id='cp-mojisuu'></div>" );
+        $( "#cp-mojisuu" ).hide();
+
         if( location.pathname == "/lms/error/notLogin" || location.pathname == "/lms/error/" ) {
             location.pathname = "/lms/lginLgir/";
         }
 
-        if( items.cpex_disp_size ) $("#cp-mojisuu").css("font-size", String(items.cpex_disp_size) );
 
-        if( items.cpex_name ) document.forms.logoutActionForm.getElementsByTagName("li")[0].innerHTML = items.cpex_name;
+        if( items.cpex_name && !document.forms.logoutActionForm.getElementsByTagName("li")[0].id ) document.forms.logoutActionForm.getElementsByTagName("li")[0].innerHTML = items.cpex_name;
 
         document.title = items.cpex_title || document.title;
 
         if( items.cpex_disp_x ){
-            if( items.cpex_disp_x == "left" ) $("#cp-mojisuu").css("left", "0").css("right", "auto");
-            else if( items.cpex_disp_x == "right" ) $("#cp-mojisuu").css("right", "0").css("left", "auto");
+            console.log(items.cpex_disp_x);
+            if( items.cpex_disp_x == "left" ){
+                document.getElementById("cp-mojisuu").style["left"] = 0;
+                document.getElementById("cp-mojisuu").style.right = "auto";
+            }
+            else if( items.cpex_disp_x == "right" ){
+                document.getElementById("cp-mojisuu").style["right"] = 0;
+                document.getElementById("cp-mojisuu").style.left = "auto";
+            }
         }
         if( items.cpex_disp_y ){
-            if( items.cpex_disp_y == "top" ) $("#cp-mojisuu").css("top", "0").css("bottom", "auto");
-            else if( items.cpex_disp_y == "bottom" ) $("#cp-mojisuu").css("bottom", "0").css("top", "auto");
+            if( items.cpex_disp_y == "top" ){
+                document.getElementById("cp-mojisuu").style["top"] = 0;
+                document.getElementById("cp-mojisuu").style.bottom = "auto";
+            }
+            else if( items.cpex_disp_y == "bottom" ){
+                document.getElementById("cp-mojisuu").style["bottom"] = 0;
+                document.getElementById("cp-mojisuu").style.top = "auto";
+            }
         }
-
-        $( "body" ).append( "<div id='cp-mojisuu'></div>" );
-        $( "#cp-mojisuu" ).hide();
+        if( items.cpex_disp_size ) $("#cp-mojisuu").css("font-size", String(items.cpex_disp_size) );
 
         $( "textarea" ).on( "click keyup", function() {
             $( "#cp-mojisuu" ).show();
@@ -33,9 +46,9 @@ chrome.storage.sync.get( null, function( items ) {
         } );
         
         if( items.cpex_open == "true" ){
-            if( $(".cs_Bt")[1].value == "すべて開く" || $(".cs_Bt")[1].value == "Open all" ) $(".cs_Bt")[1].click();
+            if( $(".cs_Bt")[1] && $(".cs_Bt")[1].value == "すべて開く" || $(".cs_Bt")[1].value == "Open all" ) $(".cs_Bt")[1].click();
         }
-        
-        if( items.cpex_attend == "true" && $("#attend")[0].value == "出席する" ) $("#attend")[0].click();
+
+        if( items.cpex_attend == "true" && $("#attend").last()[0] && $("#attend").last()[0].value == "出席する" ) $("#attend").last()[0].click();
     }
 } );
